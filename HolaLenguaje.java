31b2e5957a5f95f80f6ae070d91e140546d10b4e
Albert.eng.dev <albert.eng.dev@gmail.com>
2025-09-25 12:43:57 -0500

    nueva clase HolaLenguage.java

---
+++ HolaLenguaje.java
@@ -0,0 +1,18 @@
+import java.util.Optional; 
+
+public class HolaLenguaje {
+    public static void main(String[] args) {
+
+        // Obtiene la variable de entorno "USERNAME"        
+		String username = System.getenv("USERNAME");  
+
+        // Obtiene la variable de entorno "LANGUAGE"        
+		String language = System.getenv("LANGUAGE"); 
+
+        // Si no se encuentra, establece un valores por defectos a la variables        
+		username = Optional.ofNullable(username).orElse("Usuario"); 
+        language = Optional.ofNullable(username).orElse("Java"); 
+
+        System.out.println("¡Hola "+ username + ", desde GitHub Actions, mi lenguaje de programacion favorito es "+language);
+    }
+}
